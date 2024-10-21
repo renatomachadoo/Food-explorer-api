ece12d16d6467641b469ee9ab217e505af7c96be
Renato Machado <renatomachado176@gmail.com>
2024-10-21 09:31:53 +0100

    CORS

--- src/server.js
+++ src/server.js
@@ -4,10 +4,12 @@ require("dotenv/config")
 const AppError = require("./utils/AppError")
 const uploadConfig = require("./configs/upload")
 
+const cors = require("cors")
 const express = require("express")
 const routes = require("./routes")
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
